61b45885897d4a8a2d143d684a925c9585662336
fur <2866008@mail.ru>
2025-02-07 20:01:26 +0300

    Новый метод SimpleCalculatorGrid

--- src/main/java/org/example/SimpleCalculatorGrid.java
+++ src/main/java/org/example/SimpleCalculatorGrid.java
@@ -1,28 +1,33 @@
 package org.example;
-
 import javax.swing.*;
-import java.awt.*;
+import java.awt.GridLayout;
 
-public class Calculator {
-   public void initializeGUI() {
-       JButton myButton = new JButton();
-    JFrame myFrame = new JFrame();
-    FlowLayout f1 = new FlowLayout();
-    JPanel windowContent = new JPanel(f1);
 
-    // создаем компоненты в ппамяти
-   JLabel label1 = new JLabel("Nomer 1 :");
-   JTextField field1 = new JTextField(10);
+public class SimpleCalculatorGrid {
+public static void simpl (){
+    // создаем панель
+    JPanel windowContent = new JPanel();
+    // задаем менеджер расположения для этой панели
+    GridLayout g1 = new GridLayout(4,2);
+    windowContent.setLayout(g1);
+
+    // создаем компоненты в памяти
+
+    JLabel label1 = new JLabel("Nomer 1 :");
+    JTextField field1 = new JTextField(10);
     JLabel label2 = new JLabel("Nomer 2 :");
     JTextField field2 = new JTextField(10);
     JLabel label3 = new JLabel("Summa :");
     JTextField field3 = new JTextField(10);
     JTextField result = new JTextField(10);
     JButton go = new JButton("Итог");
-    //  добовляем компоненты на панель
+
+    // Добовляем компоненты в панель
 
     windowContent.add (label1);
+    windowContent.add (field1);
     windowContent.add (label2);
+    windowContent.add (field2);
     windowContent.add (label3);
     windowContent.add (result);
     windowContent.add (go);
@@ -34,5 +39,5 @@ public class Calculator {
 
     frame.setSize (600,300);
     frame.setVisible (true);
-    }
+}
 }
